6dc08d24ceaae270165363630b3be84deb4ba3b6
PumpkinSeed <qwer.kocka@gmail.com>
2017-12-31 13:42:38 +0100

    Add get all words from posts

--- words.go
+++ words.go
@@ -1,20 +1,59 @@
 package recommender
 
 import (
+	"fmt"
 	"strings"
 )
+
+type Post struct {
+	Title string
+	Body  string
+}
 
 type replace struct {
 	new string
 	old string
 }
 
-func getWords(body string) []string {
-	var result []string
+func Train(posts []Post) {
+	var wordsContainer [][]string
+	for _, post := range posts {
+		wordsContainer = append(wordsContainer, getWordsOfPost(post.Body))
+	}
+	words := getAllWords(wordsContainer)
 
-	body = replacer(body)
-	result = strings.Split(body, " ")
-	return result
+	fmt.Println(len(words))
+
+}
+
+func getWordsOfPost(body string) []string {
+	var wordsUniq []string
+	var wordsMap = make(map[string]bool)
+	body = replacer(strings.ToLower(body))
+	words := strings.Split(body, " ")
+
+	for _, word := range words {
+		wordsMap[word] = true
+	}
+	for k := range wordsMap {
+		wordsUniq = append(wordsUniq, k)
+	}
+	return wordsUniq
+}
+
+func getAllWords(container [][]string) []string {
+	var wordsUniq []string
+	var wordsMap = make(map[string]bool)
+
+	for _, words := range container {
+		for _, word := range words {
+			wordsMap[word] = true
+		}
+	}
+	for k := range wordsMap {
+		wordsUniq = append(wordsUniq, k)
+	}
+	return wordsUniq
 }
 
 func replacer(body string) string {
@@ -53,6 +92,10 @@ func replacer(body string) string {
 		},
 		replace{
 			old: "?",
+			new: "",
+		},
+		replace{
+			old: ":",
 			new: "",
 		},
 	}
